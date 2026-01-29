e8b1e629033d0bc9a0eff1424c942749db00c216
Your Name <you@example.com>
2026-01-29 18:42:31 +0900

    Bug fix

--- DRsim/DRsim.cc
+++ DRsim/DRsim.cc
@@ -10,20 +10,13 @@
 #include "FTFP_BERT.hh"
 #include "Randomize.hh"
 
-#ifdef G4VIS_USE
 #include "G4VisExecutive.hh"
-#endif
-
-#ifdef G4UI_USE
 #include "G4UIExecutive.hh"
-#endif
 
 int main(int argc, char** argv) {
-  #ifdef G4UI_USE
   // Detect interactive mode (if no arguments) and define UI session
   G4UIExecutive* ui = 0;
   if ( argc == 1 ) ui = new G4UIExecutive(argc, argv);
-  #endif
 
   G4int seed = 0;
   G4String filename;
@@ -35,7 +28,7 @@ int main(int argc, char** argv) {
 
   // Construct the default run manager
   auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
-
+  
   // Mandatory user initialization classes
   runManager->SetUserInitialization(new DRsimDetectorConstruction());
 
@@ -43,21 +36,22 @@ int main(int argc, char** argv) {
   G4VModularPhysicsList* physicsList = new FTFP_BERT;
   G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics();
   physicsList->RegisterPhysics(opticalPhysics);
+
   auto* optParams = G4OpticalParameters::Instance();
+  optParams->SetBoundaryInvokeSD(true);
   optParams->SetProcessActivation("Cerenkov", true);
   optParams->SetProcessActivation("Scintillation", true);
   optParams->SetCerenkovTrackSecondariesFirst(true);
   optParams->SetScintTrackSecondariesFirst(true);
+
   runManager->SetUserInitialization(physicsList);
 
-  // User action initialization
   runManager->SetUserInitialization(new DRsimActionInitialization(seed,filename));
 
   // Visualization manager construction
-  #ifdef G4VIS_USE
   G4VisManager* visManager = new G4VisExecutive;
-  visManager->Initialize();
-  #endif
+  if ( argc == 1 ) visManager->Initialize();
+
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
   if ( argc != 1 ) {
@@ -66,17 +60,12 @@ int main(int argc, char** argv) {
     G4String fileName = argv[1];
     UImanager->ApplyCommand(command+fileName);
   } else {
-    #ifdef G4UI_USE
-    #ifdef G4VIS_USE
-    UImanager->ApplyCommand("/control/execute init_vis.mac");
-    #else
-    UImanager->ApplyCommand("/control/execute init.mac");
-    #endif
+    if ( argc == 1 ) UImanager->ApplyCommand("/control/execute init_vis.mac");
+    if ( !(argc == 1) ) UImanager->ApplyCommand("/control/execute init.mac");
     if (ui->IsGUI()) { UImanager->ApplyCommand("/control/execute gui.mac"); }
     // start interactive session
     ui->SessionStart();
     delete ui;
-    #endif
   }
 
   // Job termination
@@ -84,9 +73,7 @@ int main(int argc, char** argv) {
   // owned and deleted by the run manager, so they should not be deleted
   // in the main() program !
 
-  #ifdef G4VIS_USE
   delete visManager;
-  #endif
   delete runManager;
 
   return 0;
